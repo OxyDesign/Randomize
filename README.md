f9d28c7221656a3d698a3b026e9e0a7e78b21bc9
Escoffier Nicolas <nico.oxydesign@free.fr>
2014-08-17 02:05:58 +0200

    Avoid emptying of the original array & allow to (optionaly) assign the new randomized array to the original array

--- README.md
+++ README.md
@@ -3,4 +3,5 @@ Randomize
 
 Randomize array
 
-randomize() takes an array as argument and returns a new array randomized (and empties inital array)
+randomize() takes an array as first argument (mandatory) and returns a new array randomized.
+If the second argument (optional) is set to true, it assigns the new randomized array to the original array.
--- src/randomize.js
+++ src/randomize.js
@@ -1,11 +1,15 @@
-function randomize(arr){
-	var newArray = [];
+function randomize(arr,reset){
+	var cloneArray = arr.slice(0), // avoid altering the original array (arr)
+		newArray = [];
 
-	for(var i = arr.length; i > 0; i--) {
+	for(var i = cloneArray.length; i > 0; i--) {
 		var rand = Math.round(Math.random()*(i-1));
-		newArray.push(arr[rand]);
-		arr.splice(rand,1);
+		newArray.push(cloneArray[rand]);
+		cloneArray.splice(rand,1);
 	};
+
+	// if reset is true : assign the new array (newArray) to the original array (arr)
+	if(reset) arr = newArray;
 	return newArray;
 };
 
